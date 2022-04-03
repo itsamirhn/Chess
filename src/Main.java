7523e7e07f3ae1e89727da1c5680b0bd110c4402
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-04-03 16:06:44 +0430

    Main testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import com.amirhn.GUI.ChessFrame;
 import com.amirhn.Game.Chess;
+import com.amirhn.Moves.Move;
 import com.amirhn.Moves.MoveType;
 import com.amirhn.Pieces.PieceType;
 
@@ -9,24 +10,21 @@ public class Main {
     public static void main(String[] args) {
 //        ChessFrame chessFrame = new ChessFrame();
         Chess chess = new Chess();
-        chess.setupCustomScenario();
-        System.out.println(chess);
-        System.out.println(chess.getAllowedMoves());
-//        while (chess.getAllowedMoves().stream().noneMatch(move -> move.type == MoveType.CASTLING)) {
-//            if (chess.isCheckmate()) {
-//                System.out.println("CHECKMATE");
-//                break;
-//            }
-//            if (chess.isStalemate()) {
-//                System.out.println("STALEMATE");
-//                break;
-//            }
-//            System.out.println();
-//            var moves = chess.getAllowedMoves();
-//            var move = moves.get((int) (Math.random() * moves.size()));
-//            System.out.println(move);
-//            chess.applyMove(move);
-//            System.out.println(chess);
-//        }
+        while (!chess.isInCheck()) {
+            if (chess.isCheckmate()) {
+                System.out.println("CHECKMATE");
+                break;
+            }
+            if (chess.isStalemate()) {
+                System.out.println("STALEMATE");
+                break;
+            }
+            System.out.println();
+            var moves = chess.getAllowedMoves();
+            var move = moves.get((int) (Math.random() * moves.size()));
+            System.out.println(move);
+            chess.applyMove(move);
+            System.out.println(chess);
+        }
     }
 }
