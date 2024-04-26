5c4ad0bcb971f149eb14be341be1ef9a039f42ee
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2024-04-26 21:35:51 +0330

    feat: Add apply random move to game menu

--- src/com/amirhn/GUI/ChessMenuBar.java
+++ src/com/amirhn/GUI/ChessMenuBar.java
@@ -10,17 +10,21 @@ public class ChessMenuBar extends JMenuBar implements ActionListener {
 
     private final JMenuItem newGame;
     private final JMenuItem loadFEN;
+    private final JMenuItem applyRandomMove;
 
     public ChessMenuBar(ChessMenuController controller) {
         this.controller = controller;
 
         JMenu gameMenu = new JMenu("Game");
 
-        newGame = new JMenuItem("New Game");
+        newGame = new JMenuItem("New Game (n)");
         newGame.addActionListener(this);
 
-        loadFEN = new JMenuItem("Load FEN");
+        loadFEN = new JMenuItem("Load FEN (l)");
         loadFEN.addActionListener(this);
+
+        applyRandomMove = new JMenuItem("Apply Random Move (r)");
+        applyRandomMove.addActionListener(this);
 
         gameMenu.add(newGame);
         gameMenu.add(loadFEN);
@@ -33,6 +37,8 @@ public class ChessMenuBar extends JMenuBar implements ActionListener {
             controller.newGame();
         } else if (e.getSource() == loadFEN) {
             controller.loadFEN();
+        } else if (e.getSource() == applyRandomMove) {
+            controller.applyRandomMove();
         }
     }
 }
--- src/com/amirhn/GUI/ChessMenuController.java
+++ src/com/amirhn/GUI/ChessMenuController.java
@@ -3,4 +3,5 @@ package com.amirhn.GUI;
 public interface ChessMenuController {
     void newGame();
     void loadFEN();
+    void applyRandomMove();
 }
