4499e7d2e2b1d7ca8c36ccc56361b5b357fe0395
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2024-06-05 19:37:28 +0330

    lint: Add google checkstyle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 import com.amirhn.GUI.ChessFrame;
 
-public static void main(String[] args) {
+public class Main {
+  public static void main(String[] args) {
     // GUI
     ChessFrame chessFrame = new ChessFrame();
 
     // Console
-//    Chess chess = new Chess();
-//    chess.applyMove(chess.moveFromString("Pe2e4"));
-//    System.out.println(chess);
-
+    //        Chess chess = new Chess();
+    //        chess.applyMove(chess.moveFromString("Pe2e4"));
+    //        System.out.println(chess);
+  }
 }
